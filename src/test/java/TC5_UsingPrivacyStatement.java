90763161f9e00ccc189195502f0ccf8900f54b83
Gábor Balázs Uhrin <uhringabor@gmail.com>
2021-07-26 19:37:49 +0200

    TC5_UsingPrivacyStatement added

---
+++ src/test/java/TC5_UsingPrivacyStatement.java
@@ -0,0 +1,13 @@
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Epic("Your Store webapplication user administration")
+@Feature("User registration")
+@Story("Open privacy statement and check the checkbox")
+public class TC5_UsingPrivacyStatement extends BaseTest{
+    @Test
+    @DisplayName("Open privacy statement and check the checkbox")
+}
