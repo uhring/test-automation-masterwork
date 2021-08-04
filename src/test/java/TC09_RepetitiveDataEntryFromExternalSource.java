c36b054a5c01cfd29983dea69c6753f14b6f98a7
Gábor Balázs Uhrin <uhringabor@gmail.com>
2021-08-04 17:04:38 +0200

    Refactored : TC09

--- src/test/java/TC09_RepetitiveDataEntryFromExternalSource.java
+++ src/test/java/TC09_RepetitiveDataEntryFromExternalSource.java
@@ -1,7 +1,6 @@
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
@@ -13,12 +12,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Epic("Your Store web application user administration")
 @Feature("Address book administration")
-@Story("Add multiple new addresses to an existing account from .csv file")
+@Story("Add multiple new addresses to an existing account from .csv file. File source: File path: src/test/resources/output.txt")
 public class TC09_RepetitiveDataEntryFromExternalSource extends BaseTest {
 
-  @ParameterizedTest(name = "Add new address with first name: {0}, last name: {1}, address: {2}, city: {3}, post code: {4}, country: {5}.")
+  @ParameterizedTest(name = "Add new address : First name = {0}, Last name = {1}, Address = {2}, City = {3}, Post code = {4}, Country = {5}.")
   @CsvFileSource(resources = "/addresses.csv", numLinesToSkip = 1)
-  @DisplayName("Add multiple new addresses to an existing account from .csv file. File source: File path: src/test/resources/output.txt")
   public void addMultipleAddressesToAnAccount(String firstName, String lastName, String address, String city, String postCode, String country) {
     homePage.openHomePage();
     makeScreenshot();
@@ -30,6 +28,8 @@ public class TC09_RepetitiveDataEntryFromExternalSource extends BaseTest {
     makeScreenshot();
     LOG.info("screenshot taken");
     myAccountPage.goToAddressBook();
+    makeScreenshot();
+    LOG.info("screenshot taken");
     List<WebElement> initialAddresses = driver.findElements(By.tagName("tr"));
     addressBookPage.navigateToAddAddressPage();
     makeScreenshot();
